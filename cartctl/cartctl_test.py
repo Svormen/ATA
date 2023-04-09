a31d6570463b8eba60a648a42a2a9b423f2a16fa
Slavomír Svorada <ssvorada@MacBook-Pro-uzivatela-ssvorada.local>
2023-04-09 12:53:54 +0200

    add all CEG tests

--- cartctl/cartctl_test.py
+++ cartctl/cartctl_test.py
@@ -5,7 +5,7 @@ Example of usage/test of Cart controller implementation.
 
 import sys
 from cartctl import CartCtl, Status as StatusC
-from cart import Cart, CargoReq, Status
+from cart import Cart, CargoReq, Status, CartError
 from jarvisenv import Jarvis
 import unittest
 
@@ -103,7 +103,7 @@ class TestCartRequests(unittest.TestCase):
         #self.assertEqual(cart_dev.pos, 'C')
 
 
-    # CEG - 1. Test
+    # CEG - 1. Test (without request)
     def testWithoutRequest(self):
         "testWithoutRequest"
         def on_move(c: Cart):
@@ -130,7 +130,7 @@ class TestCartRequests(unittest.TestCase):
         self.assertEqual([None, None, None, None], cart_dev.slots)
         self.assertEqual('A', cart_dev.pos)
 
-    # CEG - 2. Test
+    # CEG - 2. Test (without priority)
     def testWithNormalRequest(self):
         "testWithNormalRequest"
 
@@ -192,10 +192,153 @@ class TestCartRequests(unittest.TestCase):
         Jarvis.run()
 
         # Verify direct output
-        log(cart_dev)
         self.assertTrue(cart_dev.empty())
         self.assertEqual(Status.Idle, cart_dev.status)
         self.assertEqual('B', cart_dev.pos)
+
+    # CEG - 3. Test (with priority)
+    def testWithPriorityRequest(self):
+        "testWithPriorityRequest"
+
+        def add_load(c: CartCtl, cargo_req: CargoReq):
+            "callback for schedulled load"
+            log('%d: Requesting %s at %s' % \
+                (Jarvis.time(), cargo_req, cargo_req.src))
+            c.request(cargo_req)
+
+        def on_move(c: Cart):
+            "example callback (for assert)"
+            # put some asserts here
+            log('%d: Cart is moving %s->%s' % (Jarvis.time(), c.pos, c.data))
+
+        def on_load(c: Cart, cargo_req: CargoReq):
+            "example callback for logging"
+            self.assertTrue(cargo_req.prio)
+            self.assertGreaterEqual(Jarvis.time(), cargo_req.born + 60)
+            self.assertLess(Jarvis.time(), cargo_req.born + 120)
+            self.assertIn(cargo_req, cart_dev.slots)
+            self.assertTrue(cargo_req.onload)
+            log('%d: Cart at %s: loading: %s' % (Jarvis.time(), c.pos, cargo_req))
+            log(c)
+            cargo_req.context = "loaded"
+
+        def on_unload(c: Cart, cargo_req: CargoReq):
+            "example callback (for assert)"
+            # put some asserts here
+            #self.assertNotIn(cargo_req, c.slots)
+            if cargo_req.content == 'helmet':
+                self.assertEqual('B', c.pos)
+                self.assertEqual('helmet', cargo_req.content)
+            if cargo_req.content == 'heart':
+                self.assertEqual('A', c.pos)
+                self.assertEqual('heart', cargo_req.content)
+
+            self.assertTrue(cargo_req.onunload)
+            log('%d: Cart at %s: unloading: %s' % (Jarvis.time(), c.pos, cargo_req))
+            log(c)
+            cargo_req.context = 'unloaded'
+            
+        # Setup Cart
+        # 4 slots, 150 kg max payload capacity, 2=max debug
+        cart_dev = Cart(4, 150, 0)
+        cart_dev.onmove = on_move
+
+        # Setup Cart Controller
+        c = CartCtl(cart_dev, Jarvis)
+
+        # Setup Cargo to move
+        helmet = CargoReq('A', 'B', 30, 'helmet')
+        helmet.onunload = on_unload
+
+        heart = CargoReq('C', 'A', 140, 'heart')
+        heart.onunload = on_unload
+
+        braceletL = CargoReq('D', 'C', 30, 'braceletL')
+        braceletL.onload = on_load
+
+        # Setup Plan
+        Jarvis.reset_scheduler()
+        #         when  event     called_with_params
+        Jarvis.plan(10, add_load, (c,helmet))
+        Jarvis.plan(20, add_load, (c,heart))
+        Jarvis.plan(40, add_load, (c,braceletL))
+        
+        # Exercise + Verify indirect output
+        #   Here, we run the plan.
+        Jarvis.run()
+
+        # Verify direct output
+        self.assertTrue(cart_dev.empty())
+        self.assertEqual(Status.Idle, cart_dev.status)
+        self.assertEqual('C', cart_dev.pos)
+
+    # CEG - 4. Test (problem with slot capacity)
+    def testOnCapacity(self):
+        "testOnCapacity"
+        def add_load(c: CartCtl, cargo_req: CargoReq):
+            "callback for schedulled load"
+            log('%d: Requesting %s at %s' % \
+                (Jarvis.time(), cargo_req, cargo_req.src))
+            c.request(cargo_req)
+
+        def on_move(c: Cart):
+            "example callback (for assert)"
+            # put some asserts here
+            self.assertEqual(c.load_capacity, 150)
+            log('%d: Cart is moving %s->%s' % (Jarvis.time(), c.pos, c.data))
+
+        def on_load(c: Cart, cargo_req: CargoReq):
+            "example callback for logging"
+
+            log('%d: Cart at %s: loading: %s' % (Jarvis.time(), c.pos, cargo_req))
+            log(c)
+            cargo_req.context = "loaded"
+
+        def on_unload(c: Cart, cargo_req: CargoReq):
+            "example callback (for assert)"
+            # put some asserts here
+            log('%d: Cart at %s: unloading: %s' % (Jarvis.time(), c.pos, cargo_req))
+            log(c)
+
+        # Setup Cart
+        # 2 slots, 150 kg max payload capacity, 2=max debug
+        cart_dev = Cart(2, 150, 0)
+        cart_dev.onmove = on_move
+
+        # Setup Cart Controller
+        c = CartCtl(cart_dev, Jarvis)
+
+        # Setup Cargo to move
+        helmet = CargoReq('A', 'C', 70, 'helmet')
+        helmet.onload = on_load
+        helmet.onunload = on_unload
+
+        heart = CargoReq('A', 'C', 80, 'heart')
+        heart.onload = on_load
+        heart.onunload = on_unload
+
+        braceletR = CargoReq('C', 'B', 80, 'braceletR')
+        braceletR.onload = on_load
+        braceletR.onunload = on_unload
+
+        braceletL = CargoReq('C', 'B', 70, 'braceletL')
+        braceletL.onload = on_load
+        braceletL.onunload = on_unload
+
+        # Setup Plan
+        Jarvis.reset_scheduler()
+        #         when  event     called_with_params
+        Jarvis.plan(10, add_load, (c,helmet))
+        Jarvis.plan(15, add_load, (c,heart))
+        Jarvis.plan(55, add_load, (c,braceletR))
+        Jarvis.plan(25, add_load, (c,braceletL))
+        
+        # Exercise + Verify indirect output
+        #   Here, we run the plan.
+        Jarvis.run()
+
+        # Verify direct output
+        self.assertEqual(None, heart.context)
 
 if __name__ == "__main__":
     unittest.main()
